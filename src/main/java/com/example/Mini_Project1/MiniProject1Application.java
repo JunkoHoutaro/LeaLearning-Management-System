1d447b5dec69f08782f11f5fd58a32f79e8cee76
Test_repo <22520992@gm.uit.edu.vn>
2024-12-17 10:35:47 +0700

    Transfer confidential information to the .env file.

--- src/main/java/com/example/Mini_Project1/MiniProject1Application.java
+++ src/main/java/com/example/Mini_Project1/MiniProject1Application.java
@@ -1,5 +1,6 @@
 package com.example.Mini_Project1;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MiniProject1Application {
 
   public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+    dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+    );
+
     SpringApplication.run(MiniProject1Application.class, args);
   }
 }
